6b1658e1cb5c3d554799edb18e8dd8e1aa4894ba
Dudis2000 <122548078+Dudis2000@users.noreply.github.com>
2023-02-14 19:58:59 -0300

    Add files via upload

---
+++ Mobile Gamer/Project-template-DEVICE-COMPATIBLE-GAME-main/sketch.js
@@ -0,0 +1,163 @@
+var path,boy,cash,diamonds,jwellery,sword;
+var pathImg,boyImg,cashImg,diamondsImg,jwelleryImg,swordImg;
+var treasureCollection = 0;
+var cashG,diamondsG,jwelleryG,swordGroup;
+
+
+
+//Estados do jogo
+var PLAY=1;
+var END=0;
+var gameState=1;
+
+function preload(){
+  pathImg = loadImage("Road.png");
+  boyImg = loadAnimation("Runner-1.png","Runner-2.png");
+  cashImg = loadImage("cash.png");
+  diamondsImg = loadImage("diamonds.png");
+  jwelleryImg = loadImage("jwell.png");
+  swordImg = loadImage("sword.png");
+  endImg =loadAnimation("gameOver.png");
+}
+
+function setup(){
+  
+createCanvas(windowWidth, windowHeight);
+
+// Movendo plano de fundo
+path=createSprite(width/2, height/2);
+path.addImage(pathImg);
+path.velocityY = 4;
+
+//porque no caso a divisao por 2 sera sempre o centro e apartir dai nao existe
+//uma numero que seja adjacente para x e y como -1 seria um valor y para esqueda
+//entao no caso quando nos colocamos - ou + uma quantia ele ja captara como
+//se voce estivesse referenciando o centro, ou sempre e definido u valor do cento centro
+//para a altura e largura e mesmo assim se voce quiser que o objeto fique
+//no centro voce tem que referenciar o meio co o dividido por dois pois se colocar
+//a largura ou a altura ele nao le como centralizada ou é MAGICA
+
+
+//criar menino correndo 
+boy = createSprite(width/2, height -100);
+boy.addAnimation("SahilRunning",boyImg);
+boy.scale=0.08;
+  
+  
+cashG=new Group();
+diamondsG=new Group();
+jwelleryG=new Group();
+swordGroup=new Group();
+
+}
+
+function draw() {
+
+  if(gameState===PLAY){
+  background(0);
+  boy.x = World.mouseX;
+  
+  edges= createEdgeSprites();
+  boy.collide(edges);
+  
+  //código para redefinir plano de fundo
+  if(path.y > height ){
+    path.y = height/2;
+  }
+  
+    createCash();
+    createDiamonds();
+    createJwellery();
+    createSword();
+
+    if (cashG.isTouching(boy)) {
+      cashG.destroyEach();
+      treasureCollection=treasureCollection+50;
+    }
+    else if (diamondsG.isTouching(boy)) {
+      diamondsG.destroyEach();
+      treasureCollection=treasureCollection+100;
+      
+    }else if(jwelleryG.isTouching(boy)) {
+      jwelleryG.destroyEach();
+      treasureCollection= treasureCollection + 150;
+      
+    }else{
+      if(swordGroup.isTouching(boy)) {
+        gameState=END;
+        
+        boy.addAnimation("SahilRunning",endImg);
+        boy.x = width/2;
+        boy.y = height/2;
+        boy.scale=0.6;
+        
+        cashG.destroyEach();
+        diamondsG.destroyEach();
+        jwelleryG.destroyEach();
+        swordGroup.destroyEach();
+
+        //Na minha aula de ciencia eu aprendi que minerios nao sao
+        //seres vivos, minha vida foi uma mentira
+        //Os minerios podem ter sentimentos profundos, emocionate
+        //nunca mais vou minerar no minecraft, os minerios vao chorar e ser separados de 
+        //suas familias :(
+        
+        cashG.setVelocityYEach(0);
+        diamondsG.setVelocityYEach(0);
+        jwelleryG.setVelocityYEach(0);
+        swordGroup.setVelocityYEach(0);
+     
+    }
+  }
+  
+  drawSprites();
+  textSize(20);
+  fill(255);
+  text("Tesouro: "+ treasureCollection,150,30);
+  }
+
+}
+
+function createCash() {
+  if (World.frameCount % 200 == 0) {
+  var cash = createSprite(Math.round(random(50, width-50),40, 10, 10));
+  cash.addImage(cashImg); //acertei 50%
+  cash.scale=0.12;
+  cash.velocityY = 3;
+  cash.lifetime = 150;
+  cashG.add(cash);
+  }
+}
+
+function createDiamonds() {
+  if (World.frameCount % 320 == 0) {
+  var diamonds = createSprite(Math.round(random(50, width-50),40, 10, 10));
+  diamonds.addImage(diamondsImg);
+  diamonds.scale=0.03;
+  diamonds.velocityY = 3;
+  diamonds.lifetime = 150;
+  diamondsG.add(diamonds);
+}
+}
+
+function createJwellery() {
+  if (World.frameCount % 410 == 0) {
+  var jwellery = createSprite(Math.round(random(50, width-59),40, 10, 10));
+  jwellery.addImage(jwelleryImg);
+  jwellery.scale=0.13;
+  jwellery.velocityY = 3;
+  jwellery.lifetime = 150;
+  jwelleryG.add(jwellery);
+  }
+}
+
+function createSword(){
+  if (World.frameCount % 530 == 0) {
+  var sword = createSprite(Math.round(random(50, width-50),40, 10, 10));
+  sword.addImage(swordImg);
+  sword.scale=0.1;
+  sword.velocityY = 3;
+  sword.lifetime = 150;
+  swordGroup.add(sword);
+  }
+}
